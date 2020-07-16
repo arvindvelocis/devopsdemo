4bcd9423bb422231ebad3b392e56d966f13481a3
Arvind Gupta <arvind1@Arvinds-MacBook-Air.local>
2020-07-17 02:18:50 +0530

    This is my third  commit

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ require('http')
   .createServer((req, res) => {
     console.log(`incoming url: ${req.url} and incoming method: ${req.method}`)
     res.writeHeader(200,{'Content-Type': 'text/html'})
-    res.write('<p>Buenos dias Montevideo</p>')
-    res.write('<p>This is Saturday. Bla blah blah</p>')
-    res.end('<h1>Hello World Node University course AWS Intermediate</h1>')
+    res.write('<p>Welcome to Knowledge Management Session on DevOps</p>')
+    res.write('<p>AWS Code Pipeline</p>')
+    res.end('<h1>DevOps Team</h1>')
   })
   .listen(port, (error)=>{
     console.log(`server is running on ${port}`)
