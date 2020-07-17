abb52c458149d3d1af27bbc5afdf47f309ee0eab
Arvind Gupta <arvind1@Arvinds-MacBook-Air.local>
2020-07-17 16:03:03 +0530

    demo commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('http')
     res.writeHeader(200,{'Content-Type': 'text/html'})
     res.write('<h1>Welcome to Knowledge Management Session on DevOps</h1>')
     res.write('<h2>AWS Code Pipeline</h2>')
-    res.end('<h3>Velocis DevOps Team</h3>')
+    res.end('<h3>A demo session is going on DevOps.</h3>')
   })
   .listen(port, (error)=>{
     console.log(`server is running on ${port}`)
